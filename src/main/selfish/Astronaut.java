ab3d5c42fa9721b627e8cbdaeffbd20a1003d48d
AYUSH GUPTA <ayush.gupta-2@student.manchester.ac.uk>
2023-05-02 01:40:22 +0100

    c

--- src/main/selfish/Astronaut.java
+++ src/main/selfish/Astronaut.java
@@ -27,7 +27,7 @@ public class Astronaut implements java.io.Serializable {
     public void addToTrack(Card card){track.add(card);}
     public int breathe(){for (int i = 0; i < oxygens.size(); i++){
                         if (oxygens.get(i).getValue() == 1){oxygens.remove(i);  return oxygenRemaining();}}
-                        oxygens.remove(oxygens.size()); oxygens.add(new Oxygen(1));
+                        oxygens.remove(oxygens.size()-1); oxygens.add(new Oxygen(1));
                         return oxygenRemaining();}
     public int distanceFromShip(){return (6 - track.size());}
     public List<Card> getActions(){List<Card> c = new ArrayList<Card>(actions); Collections.sort(c);return c;}
@@ -44,17 +44,20 @@ public class Astronaut implements java.io.Serializable {
                                 for (int i = 0; i < oxygens.size(); i++){
                                     if (oxygens.get(i).getValue() == ((Oxygen)card).getValue()){oxygens.remove(i); break;}}}
                                 else {for (int i = 0; i < actions.size(); i++){
-                                    if (actions.get(i).equals(card)){actions.remove(i); break;}}}}
+                                    if (actions.get(i).equals(card)){actions.remove(i); break;}}}
+                                if (oxygenRemaining() == 0){game.killPlayer(this); actions.clear();}}
     public Card hack(String card) {Card c = new Card(); if (card == "Oxygen(1)" || card == "Oxygen(2)"){
                                 for (int i = 0; i < oxygens.size(); i++){
                                     if (oxygens.get(i).toString() == card){c = oxygens.remove(i); break;}}}
                                 else {for (int i = 0; i < actions.size(); i++){
                                     if (actions.get(i).toString() == card){c = actions.remove(i); break;}}}
-                                    return c;}
+                                    if (oxygenRemaining() == 0){game.killPlayer(this); actions.clear();}
+                                    return c;
+                                }
     public int hasCard(String card){int num = 0; List<Card> c = getHand();
                                     for (Card o : c){if (card.equals(o.toString())){num++;}}      
                                     return num;}
-    public boolean hasMeltedEyeballs(){return (actions.get(actions.size()-1).toString() == "Solar flare");}
+    public boolean hasMeltedEyeballs(){List<Card> action = new ArrayList<Card>(track); if (action.size() == 0){return false;} return (action.get(action.size()-1).toString() == "Solar flare");}
     public boolean hasWon(){return (distanceFromShip() == 0 && isAlive());}
     public boolean isAlive(){if (oxygens.size() == 0){return false;} return true;}
     public Card laserBlast(){List<Card> c = new ArrayList<Card>(track); Card o = c.remove(c.size()-1); 
