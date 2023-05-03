be8329276e135df47cdc77c26a5af673cc169992
AYUSH GUPTA <ayush.gupta-2@student.manchester.ac.uk>
2023-05-03 03:05:34 +0100

    c

--- src/main/selfish/Astronaut.java
+++ src/main/selfish/Astronaut.java
@@ -115,6 +115,10 @@ public class Astronaut implements java.io.Serializable {
     public List<Card> getHand(){List<Card> c = new ArrayList<Card>();
                                 c.addAll(actions); c.addAll(oxygens); Collections.sort(c);
                                 return c;}
+    /**
+     * jvh;WVBHwvoh
+     * @return avh;WEUVOWEGV
+     */
     public String getHandStr(){List<Card> cards = this.getHand();
         String ans = "";
         int ones = 0;
@@ -146,7 +150,15 @@ public class Astronaut implements java.io.Serializable {
         }
         return (twos + singles + ans);
 }
+/**
+ * HCV;ovhb;ROVHr
+ * @return svg;WVIEBw;uoregh
+ */
     public Collection<Card> getTrack(){Collection<Card> c = new ArrayList<Card>(track); return c;}
+    /**
+     * lvh;orhgoHRohgre
+     * @param card .djvbWVHB;Whgero;igh
+     */
     public void hack(Card card){if (card == null){throw new IllegalArgumentException();}
         boolean fnd = false;
         if (card instanceof Oxygen){
@@ -156,6 +168,11 @@ public class Astronaut implements java.io.Serializable {
                                     if (actions.get(i).equals(card)){fnd = true; actions.remove(i); break;}}}
                                 if(!fnd){throw new IllegalArgumentException();}
                                 if (oxygenRemaining() == 0){game.killPlayer(this); actions.clear();} }
+    /**
+     * kjsbv;wovweibeoib
+     * @param card vhew;vhw;eghv
+     * @return vdhaw;vuirvhghewg
+     */
     public Card hack(String card) {if (card == null){throw new IllegalArgumentException();}
         boolean fnd = false;
         Card c = new Card(); if (card.equals("Oxygen(1)") || card.equals("Oxygen(2)")){
@@ -167,26 +184,67 @@ public class Astronaut implements java.io.Serializable {
                                     if (!fnd){throw new IllegalArgumentException();}
                                     return c;
                                 }
+/**
+ * ljbf;VH;ohgriehbn
+ * @param card dvh;vh;OH
+ * @return sdvh;IUVB;RV
+ */
     public int hasCard(String card){int num = 0; List<Card> c = getHand();
                                     for (Card o : c){if (card.equals(o.toString())){num++;}}      
                                     return num;}
+/**
+ * jfhb;WVB;OREV
+ * @return JDHC;veug;hrwe
+ */
     public boolean hasMeltedEyeballs(){List<Card> action = new ArrayList<Card>(track); if (action.size() == 0){return false;} return (action.get(action.size()-1).toString() == "Solar flare");}
+/**
+ * jkcavbabnbn 
+ * @return a;vh;HVW;ghwrg
+ */
     public boolean hasWon(){return (distanceFromShip() == 0 && isAlive());}
+    /**
+     * lvhaw;rghro;ihgerog
+     * @return avh;WHF;WIRGFR
+     */
     public boolean isAlive(){if (oxygens.size() == 0){return false;} return true;}
+    /**
+     * JSDFHB;OWHGw;goh
+     * @return a;vh;EWVHW;gh;rw
+     */
     public Card laserBlast(){try {List<Card> c = new ArrayList<Card>(track); Card o = c.remove(c.size()-1); 
                             track.clear(); track.addAll(c); return o;}
                         catch(IndexOutOfBoundsException e){throw new IllegalArgumentException();}}
+    /**
+     * HFAO;RHGAROEJB
+     * @return DVH;OURHVN;ORIWHFG
+     */
     public int oxygenRemaining(){int num = 0; for (Oxygen o : oxygens){num += o.getValue();} return num;}
+    /**
+     * FHWR;OHGWR;IOGH
+     * @return HCw;fhewogoghgW
+     */
     public Card peekAtTrack(){if(track.size() > 0){
                             List<Card> c = new ArrayList<Card>(track); return c.remove(c.size()-1);}
                             return null;}
+    /**
+     * dljhfawvhgohaerg
+     * @return fhvawr;uhgr
+     */
     public Oxygen siphon(){Oxygen o; for (int i = 0; i < oxygens.size(); i++){
         if (oxygens.get(i).getValue() == 1){o = oxygens.remove(i);if(oxygenRemaining() == 0){game.killPlayer(this);}return o;}}
         o = new Oxygen(1); oxygens.remove(oxygens.size()-1); oxygens.add(new Oxygen(1)); return o;}
-    public Card steal(){List<Card> c = getHand(); 
+   /**
+    * aehfoa;rghGI
+    * @return SHV;whgoewhg
+    */
+   public Card steal(){List<Card> c = getHand(); 
         Random r = new Random(); int i = r.nextInt(c.size()); Card o = c.get(i);
     if (o instanceof Oxygen){for (int j = 0; j < oxygens.size(); j++){if(o.equals(oxygens.get(j)))oxygens.remove(j); if(oxygenRemaining() == 0){game.killPlayer(this);}}}
     else {for (int j = 0; j < actions.size(); j++){if(o.equals(actions.get(j)))actions.remove(j);}} return o;}
+    /**
+     * lsvhar;ehga;rihgaergh
+     * @param swapee advhalirufhgfegf
+     */
     public void swapTrack(Astronaut swapee){List<Card> c = new ArrayList<>(this.track); this.track = swapee.track; swapee.track = c;}
 }
  
