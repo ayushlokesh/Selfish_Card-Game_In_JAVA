74d46ceb77ce60599e3fa8e34104b32bee6e4940
AYUSH GUPTA <ayush.gupta-2@student.manchester.ac.uk>
2023-05-03 01:00:00 +0100

    c

--- src/main/selfish/Astronaut.java
+++ src/main/selfish/Astronaut.java
@@ -105,22 +105,18 @@ public class Astronaut implements java.io.Serializable {
         return (twos + singles + ans);
 }
     public Collection<Card> getTrack(){Collection<Card> c = new ArrayList<Card>(track); return c;}
-    public void hack(Card card){ if (card == null){throw new IllegalArgumentException();}
-        if (card instanceof Oxygen){
+    public void hack(Card card){if (card instanceof Oxygen){
                                 for (int i = 0; i < oxygens.size(); i++){
                                     if (oxygens.get(i).getValue() == ((Oxygen)card).getValue()){oxygens.remove(i); break;}}}
                                 else {for (int i = 0; i < actions.size(); i++){
                                     if (actions.get(i).equals(card)){actions.remove(i); break;}}}
                                 if (oxygenRemaining() == 0){game.killPlayer(this); actions.clear();}}
-    public Card hack(String card) {if(card == null){throw new IllegalArgumentException();}
-        Card c = new Card(); boolean isfnd = false;
-         if (card.equals("Oxygen(1)") || card.equals("Oxygen(2)")){
+    public Card hack(String card) {Card c = new Card(); if (card.equals("Oxygen(1)") || card.equals("Oxygen(2)")){
                                 for (int i = 0; i < oxygens.size(); i++){
-                                    if (oxygens.get(i).toString().equals(card)){isfnd = true; c = oxygens.remove(i); break;}}}
+                                    if (oxygens.get(i).toString().equals(card)){c = oxygens.remove(i); break;}}}
                                 else {for (int i = 0; i < actions.size(); i++){
-                                    if (actions.get(i).toString() == card){isfnd = true;c = actions.remove(i); break;}}}
+                                    if (actions.get(i).toString() == card){c = actions.remove(i); break;}}}
                                     if (oxygenRemaining() == 0){actions.clear(); game.killPlayer(this);}
-                                    if (!isfnd){throw new IllegalArgumentException();}
                                     return c;
                                 }
     public int hasCard(String card){int num = 0; List<Card> c = getHand();
